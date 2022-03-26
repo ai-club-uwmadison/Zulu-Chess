a8845d7d2cae3536e5c6aeaa6b1f1df02219cba0
samsungplay <samsungplayee@gmail.com>
2022-03-26 15:18:44 -0500

    Revised Game.py. Should work fine now..

--- Game.py
+++ Game.py
@@ -22,6 +22,12 @@ class Game():
             return 'X'
         else:
             return '0'
+    def equals(self,obj1 : list,obj2 : list):
+        obj1  = [str(item) for item in obj1]
+        obj2 = [str(item) for item in obj2]
+        obj1 = set(obj1)
+        obj2 = set(obj2)
+        return obj1 == obj2
 
     def printBoard(self,board,player):
         board_only = board[0]
@@ -57,12 +63,12 @@ class Game():
         print()
         data = board[1]
         print('player 1 (O) remaining cows: ',int(data[0].item()))
-        print('player -1 (X) remaining cows:',int(data[1].item()))
-        print('player 1 (O) just captured row?',int(data[2].item()))
-        print('player -1 (X) just captured row?',int(data[3].item()))
-        print('player 1 (O) cow selection:',int(data[4].item()))
-        print('player -1 (X) cow selection:',int(data[5].item()))
-        print('player 1 (O) placeable cows: ',int(data[6].item()))
+        print('player -1 (X) remaining cows:',int(data[4].item()))
+        print('player 1 (O) just captured row?',int(data[1].item()))
+        print('player -1 (X) just captured row?',int(data[5].item()))
+        print('player 1 (O) cow selection:',int(data[2].item()))
+        print('player -1 (X) cow selection:',int(data[6].item()))
+        print('player 1 (O) placeable cows: ',int(data[3].item()))
         print('player -1 (X) placeable cows:',int(data[7].item()))
         print('game state: ', 'place tokens' if data[8] == 0 else 'move/fly tokens')
         print('available actions:', self.getValidMoves(board,player))
@@ -94,7 +100,9 @@ class Game():
         # board[2,7] = -1
 
         #3 squares of 3x3 (innermost, middle, outermost)
-        metadata = torch.tensor([12,12,0,0,-1,-1,12,12,0])
+        # metadata = torch.tensor([12,12,0,0,-1,-1,12,12,0])
+        metadata = torch.tensor([12,0,-1,12,12,0,-1,12,0])
+        #this player remaining tokens = data[0],  this player row captured = data[1], this player selected cow = data[2], this player placeable tokens = data[3], game state = data[-1]
         #vector that represents each player's remaining tokens (alive, not necessarily placeable) + whether each player has just captured a row + the cow selected for moving for each player + the remaining cow tokens available to place for each players + the game phase code
         return board, metadata
 
@@ -110,7 +118,7 @@ class Game():
         Returns:
             actionSize: number of all possible actions
         """
-        return torch.prod(torch.tensor(self.getInitBoard()[0].size()))
+        return torch.prod(torch.tensor(self.getInitBoard()[0].size())) + 1
 
     def getNextState(self, board, player, action, last_row_captured_count):
         """
@@ -123,12 +131,13 @@ class Game():
             nextBoard: board after applying action
             nextPlayer: player who plays in the next turn (should be -player)
         """
+
         board_only = board[0]
         data = board[1]
-        row_just_captured = data[ 2 if player == 1 else 3] == 1
-        can_fly = data[0 if player == 1 else 1] <= 3 or data[-1] == 0
-        cow_selected = data[4 if player == 1 else 5]
-        player_index = 6 if player == 1 else 7
+        row_just_captured = data[1 if player == 1 else 1+4] == 1
+        can_fly = data[0 if player == 1 else 0+4] <= 3 or data[-1] == 0
+        cow_selected = data[2 if player == 1 else 2+4]
+        # player_index = 6 if player == 1 else 7
         game_state = data[-1]
 
 
@@ -139,30 +148,30 @@ class Game():
                 cell = action - 8 * (square-1)
                 board_only[square-1,cell] = player
                 #decrement placeable count
-                data[player_index] -= 1
+                data[3 if player == 1 else 3+4] -= 1
                 print('PLACED cow on the board')
 
             if not row_just_captured and game_state == 1 and cow_selected == -1:
                 #selecting cow to move
-                data[4 if player == 1 else 5] = action
+                data[2 if player == 1 else 2+4] = action
                 print('SELECTED cow on the board')
 
                 return board, player, last_row_captured_count
             if row_just_captured:
                 # displace opponent's cow on the board
-                action = action - 24
+                # action = action - 24
                 square = min(math.ceil((action+1) / 8),3)
                 cell = action - 8 * (square - 1)
                 board_only[square-1, cell] = 0
                 # decrement the other player's remaining cows
-                data[1 if player == 1 else 0] -= 1
+                data[4 if player == 1 else 0] -= 1
                 #set row_captured to 0
-                data[2 if player == 1 else 3] = 0
+                data[1 if player == 1 else 1+4] = 0
                 print('DISPLACED cow on the board')
                 #update opponent's row counts
-                row_capture_count = {str(-player): 0, str(player): last_row_captured_count[str(player)]}
+                row_capture_count = {str(-player): [], str(player): last_row_captured_count[str(player)]}
 
-                row_capture_count[str(-player)] = 0
+                row_capture_count[str(-player)] = []
                 horizontal_vertical_patterns = [
                     [0, 1, 2],
                     [2, 3, 4],
@@ -178,14 +187,14 @@ class Game():
                             # row captured!
 
                             captured = True
-                            row_capture_count[str(-player)] += 1
+                            row_capture_count[str(-player)].append([pattern[0] * (i+1),pattern[1] * (i+1),pattern[2] * (i+1)])
                 # check captures across squares
                 for i in range(8):
                     if board_only[0][i] == -player and board_only[1][i] == -player and board_only[2][i] == -player:
                         # row captured!
 
                         captured = True
-                        row_capture_count[str(-player)] += 1
+                        row_capture_count[str(-player)].append([i,i,i])
 
                 return board, -player, row_capture_count
             if not row_just_captured and cow_selected != -1 and game_state == 1:
@@ -196,11 +205,14 @@ class Game():
                 square = min(math.ceil((action+1) / 8),3)
                 cell = action - 8 * (square - 1)
                 board_only[square-1, cell] = player
-                data[4 if player == 1 else 5] = -1
+                data[2 if player == 1 else 2+4] = -1
                 print('MOVED cow on the board')
+        else:
+            #no action
+            return board, -player, last_row_captured_count
 
         #update game state
-        if data[6] <= 0 and data[7] <= 0:
+        if data[3] <= 0 and data[7] <= 0:
             #if both players do not have any more placeable tokens, then the game phase is updated to 1 (where only moving tokens are possible)
             data[-1] = 1
             print('Game state updated')
@@ -213,9 +225,9 @@ class Game():
             [6,7,0]
         ]
         captured = False
-        row_capture_count = { str(player) : 0, str(-player) : last_row_captured_count[str(-player)]}
+        row_capture_count = { str(player) : [], str(-player) : last_row_captured_count[str(-player)]}
 
-        row_capture_count[str(player)] = 0
+        row_capture_count[str(player)] = []
         #check capture within square
         for i in range(3):
             square = board_only[i]
@@ -224,19 +236,19 @@ class Game():
                     #row captured!
 
                     captured = True
-                    row_capture_count[str(player)] += 1
+                    row_capture_count[str(player)].append([pattern[0]*(i+1),pattern[1]*(i+1),pattern[2]*(i+1)])
         #check captures across squares
         for i in range(8):
             if board_only[0][i] == player and board_only[1][i] == player and board_only[2][i] == player:
                 #row captured!
 
                 captured = True
-                row_capture_count[str(player)] += 1
+                row_capture_count[str(player)].append([i,i,i])
 
 
         print('total row capture count (for current player):',row_capture_count[str(player)], 'last row capture count (for current player):',last_row_captured_count[str(player)])
-        if row_capture_count[str(player)] >= last_row_captured_count[str(player)] and captured:
-            data[2 if player == 1 else 3] = 1
+        if captured and not self.equals(row_capture_count[str(player)],last_row_captured_count[str(player)]) and len(row_capture_count[str(player)]) >= len(last_row_captured_count[str(player)]):
+            data[1 if player == 1 else 1+4] = 1
             print('ROW CAPTURED')
             return board, player, row_capture_count
 
@@ -286,21 +298,24 @@ class Game():
         data = board[1]
         action_vector = torch.zeros((self.getActionSize(),))
         #check if the player has just captured a row
-        player_index = 2 if player == 1 else 3
-        captured_row = data[player_index] == 1
+        # player_index = 2 if player == 1 else 3
+        # print('DATA CHECK:',data)
+        captured_row = data[1] == 1
         if captured_row:
             #if a row has just been captured...
             for i in range(24):
                 square = min(math.ceil((i+1)/8),3)
                 cell = (i) - 8 * (square-1)
                 status = board_only[square-1,cell]
+
                 #any cow on the cell where it is occupied by the OTHER player can be displaced
                 if status == -player:
+                    # print('status=', status, '(',i,')')
                     action_vector[i] = 1
         else:
             #if a row has not been captured, check whether the current player can fly
-            player_index = 0 if player == 1 else 1
-            can_fly = data[player_index] <= 3 or data[-1] == 0
+            # player_index = 0 if player == 1 else 1
+            can_fly = data[0] <= 3 or data[-1] == 0
 
             #game phase 0 (placing cow anywhere)
             if can_fly and data[-1] == 0:
@@ -316,8 +331,8 @@ class Game():
             #game phase 1 (moving cow anywhere)
             elif can_fly and data[-1] == 1:
                 # first check if the player has selected a cow to move
-                player_index = 4 if player == 1 else 5
-                cow_selected = data[player_index] > 0
+                # player_index = 4 if player == 1 else 5
+                cow_selected = data[2] >= 0
                 if not cow_selected:
                     for i in range(24):
                         square = min(math.ceil((i+1)/8),3)
@@ -339,8 +354,9 @@ class Game():
                 #if cannot fly, then only the cells within one adjacent reach of the current player's cow are available
 
                 #first check if the player has selected a cow to move
-                player_index = 4 if player == 1 else 5
-                cow_selected = data[player_index] > 0
+                # player_index = 4 if player == 1 else 5
+                cow_selected = data[2] >=0
+                # print('cow selected? ', cow_selected)
                 if not cow_selected:
                     for i in range(24):
                         square = min(math.ceil((i+1)/8),3)
@@ -359,7 +375,7 @@ class Game():
 
                 else:
                     #if cow has been selected, its neighboring cells are available
-                    cow_selected = data[player_index]
+                    cow_selected = data[2]
                     neighbors = self._getNeighbor(board_only,cow_selected)
                     for neighbor in neighbors:
                         # all empty neighbors == available action
@@ -367,6 +383,10 @@ class Game():
                         cell = neighbor - 8 * (square - 1)
                         if board_only[square-1,cell] == 0:
                             action_vector[neighbor] = 1
+        no_action = torch.zeros(size=(1,))
+        if action_vector.sum() < 0.001:
+            no_action[0] = 1
+        action_vector = torch.cat([action_vector,no_action],dim=0)
         return action_vector
 
 
@@ -389,8 +409,8 @@ class Game():
 
         """
         data = board[1]
-        remaining_tokens = data[0 if player == 1 else 1]
-        remaining_tokens_other = data[1 if player == 1 else 0]
+        remaining_tokens = data[0 if player == 1 else 4]
+        remaining_tokens_other = data[4 if player == 1 else 0]
 
         win = remaining_tokens_other <= 2
         if win:
@@ -399,6 +419,8 @@ class Game():
         if loss:
             return -1
 
+        if remaining_tokens == 12 and remaining_tokens_other == 12 and data[3] == 0 and data[7] == 0:
+            return 0.1 #could rarely happen for a draw!
         return 0
 
     def getCanonicalForm(self, board, player):
@@ -417,15 +439,11 @@ class Game():
         """
         board = [torch.clone(board[0]),torch.clone(board[1])]
         if player == 1:
-            data = board[1]
-            canonical_data = torch.tensor([data[0],data[2],data[4],data[6],data[8]])
-            board[1] = canonical_data
             return board
         else:
             board[0] = board[0] * -1
             data = board[1]
-            canonical_data = torch.tensor([data[1],data[3],data[5],data[7],data[8]])
-            board[1] = canonical_data
+            board[1] = torch.tensor([data[4],data[5],data[6],data[7],data[0],data[1],data[2],data[3],data[-1]])
             return board
 
     def getSymmetries(self, board, pi):
@@ -450,12 +468,13 @@ class Game():
             boardString: a quick conversion of board to a string format.
                          Required by MCTS for hashing.
         """
+        board = self.getCanonicalForm(board,1)
         boardString = ""
         board_only = board[0]
         for i in range(24):
             square = min(math.ceil((i+1)/8),3)
             cell = i - 8 * (square - 1)
-            boardString += str(board_only[square,cell])
+            boardString += str(board_only[square-1,cell])
         data = board[1]
         for datum in data:
             boardString += str(datum)
@@ -468,55 +487,9 @@ def play():
     game = Game()
     player = 1
     board = game.getInitBoard()
-    board_only = board[0]
-    last_row_captured_count = {'1' : 0, '-1': 0}
-    # check for any row capture on current player's side
-    horizontal_vertical_patterns = [
-        [0, 1, 2],
-        [2, 3, 4],
-        [4, 5, 6],
-        [6, 7, 0]
-    ]
-    captured = False
-    row_capture_count = {str(player): 0, str(-player): 0}
 
-    row_capture_count[str(player)] = 0
-    # check capture within square
-    for i in range(3):
-        square = board_only[i]
-        for pattern in horizontal_vertical_patterns:
-            if square[pattern[0]] == player and square[pattern[1]] == player and square[pattern[2]] == player:
-                # row captured!
 
-                captured = True
-                row_capture_count[str(player)] += 1
-    # check captures across squares
-    for i in range(8):
-        if board_only[0][i] == player and board_only[1][i] == player and board_only[2][i] == player:
-            # row captured!
-
-            captured = True
-            row_capture_count[str(player)] += 1
-
-    row_capture_count[str(-player)] = 0
-    # check capture within square
-    for i in range(3):
-        square = board_only[i]
-        for pattern in horizontal_vertical_patterns:
-            if square[pattern[0]] == -player and square[pattern[1]] == -player and square[pattern[2]] == -player:
-                # row captured!
-
-                captured = True
-                row_capture_count[str(-player)] += 1
-    # check captures across squares
-    for i in range(8):
-        if board_only[0][i] == -player and board_only[1][i] == -player and board_only[2][i] == -player:
-            # row captured!
-
-            captured = True
-            row_capture_count[str(-player)] += 1
-
-    last_row_captured_count = row_capture_count
+    last_row_captured_count = {'-1': [], '1': []}
 
     while True:
         game.printBoard(board,player)
