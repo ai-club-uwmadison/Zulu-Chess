e2280d744562f56a8b1f558ed4e5fa28d79acb5f
samsungplay <samsungplayee@gmail.com>
2022-03-26 15:19:51 -0500

    Implemented Arena class. If you get ImportError just delete from .Game import Game and copy-paste the whole Game class above

--- Arena.py
+++ Arena.py
@@ -1,6 +1,13 @@
+
+
+
 import logging
 
 from tqdm import tqdm
+import numpy as np
+import torch
+import math
+from .Game import Game
 
 log = logging.getLogger(__name__)
 
@@ -26,6 +33,7 @@ class Arena():
         self.player2 = player2
         self.game = game
         self.display = display
+        self.last_row_captured_count = {'1' : [], '-1': []}
 
     def playGame(self, verbose=False):
         """
@@ -46,20 +54,26 @@ class Arena():
             if verbose:
                 assert self.display
                 print("Turn ", str(it), "Player ", str(curPlayer))
-                self.display(board)
-            action = players[curPlayer + 1](self.game.getCanonicalForm(board, curPlayer))
+                self.display(board,curPlayer)
 
             valids = self.game.getValidMoves(self.game.getCanonicalForm(board, curPlayer), 1)
+            # print('valids=', valids)
+            # print('other(actual) valids=', self.game.getValidMoves(board,curPlayer))
+            action = players[curPlayer + 1](self.game.getCanonicalForm(board, curPlayer),valids)
+
+
+            print('action chosen=',action)
 
             if valids[action] == 0:
                 log.error(f'Action {action} is not valid!')
                 log.debug(f'valids = {valids}')
                 assert valids[action] > 0
-            board, curPlayer = self.game.getNextState(board, curPlayer, action)
+            board, curPlayer, self.last_row_captured_count = self.game.getNextState(board, curPlayer, action, self.last_row_captured_count)
         if verbose:
             assert self.display
             print("Game over: Turn ", str(it), "Result ", str(self.game.getGameEnded(board, 1)))
-            self.display(board)
+            self.display(board,curPlayer)
+        print('Game end at perpective of player',curPlayer)
         return curPlayer * self.game.getGameEnded(board, curPlayer)
 
     def playGames(self, num, verbose=False):
@@ -98,3 +112,40 @@ class Arena():
                 draws += 1
 
         return oneWon, twoWon, draws
+
+
+def random_agent(board,valid_moves):
+    options = []
+    for i, action_valid in enumerate(valid_moves):
+        if action_valid == 1:
+            options.append(i)
+
+    action = np.random.choice(options)
+    return action
+
+valids = torch.tensor([0., 0., 0., 1., 1., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 1., 0.,
+        0., 0., 0., 1., 1., 0.])
+
+# for _ in range(50):
+#     print(random_agent(None,valids))
+
+game = Game()
+
+# for i in range(24):
+#     print(f'neighbor for {i}',game._getNeighbor(game.getInitBoard()[0],i))
+arena = Arena(player1=random_agent,player2=random_agent,game=Game(),display=game.printBoard)
+#
+# arena.playGame(verbose=True)
+oneWon, twoWon, draw = arena.playGames(num=1,verbose=True)
+print('-------')
+print('stats:')
+print('oneWon:',oneWon)
+print('twoWon:',twoWon)
+print('draws:',draw)
+# board = (torch.tensor([1,1,1,1,1]),torch.tensor([0,0,0]))
+# board_ = [torch.clone(board[0]),torch.clone(board[1])]
+#
+# board_[0] = -1 * board_[0]
+#
+# print(board_)
+# print(board)
